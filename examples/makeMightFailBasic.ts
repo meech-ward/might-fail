95fd7e6226e067b5f049bafb77f3c45ddb199365
sam <sam@meech-ward.me>
2023-06-09 15:07:04 -0700

    Add examples

---
+++ examples/makeMightFailBasic.ts
@@ -0,0 +1,14 @@
+import makeMightFail from "../src/makeMightFail";
+
+const resolve = (value: {message: string}) => Promise.resolve(value);
+async function main() {
+  const func = makeMightFail(resolve);
+  const { error, result } = await func({ message: "success" });
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(result.message);
+}
+
+main();
---
+++ examples/mightFailBasic.ts
@@ -0,0 +1,14 @@
+import mightFail from "../src/mightFail";
+
+async function main() {
+  const { error, result } = await mightFail(
+    Promise.resolve({ message: "success" })
+  );
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(result.message);
+}
+
+main();
