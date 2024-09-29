b328d68e69c4d7656c18146859646fab598ddb4d
sam <sam@meech-ward.me>
2024-09-29 13:47:39 -0700

    add module docs for jsr

--- src/go/index.ts
+++ src/go/index.ts
@@ -1,3 +1,21 @@
+  /**
+   * @module
+   *
+   * This module contains the interface to use the result of mightFail as an error-last tuple.
+   * 
+   * This aligns with the proposal-safe-assignment-operator: https://github.com/arthurfiorette/proposal-safe-assignment-operator?tab=readme-ov-file#why-not-data-first
+   *
+   * If you want to use error-last style, just like golang, use the `/go` module.
+   * 
++  * @example
++  * ```ts
++  * import { mightFail } from "@might/fail/tuple";
++  *
++  * const [error, result] = await mightFail(promise);
++  * ```
+   */
+
+
 import { type Either } from "./Either";
 import { mightFail, mightFailSync } from "./mightFail";
 import { makeMightFail, makeMightFailSync } from "./makeMightFail";
--- src/tuple/index.ts
+++ src/tuple/index.ts
@@ -1,3 +1,20 @@
+  /**
+   * @module
+   *
+   * This module contains the interface to use the result of mightFail as an error-first tuple.
+   * 
+   * This mimics the behaviour of golang.
+   *
+   * If you want to use error-first style, use the `/tuple` module.
+   * 
++  * @example
++  * ```ts
++  * import { mightFail } from "@might/fail/tuple";
++  *
++  * const [result, error] = await mightFail(promise);
++  * ```
+   */
+
 import { type Either } from "./Either";
 import { mightFail, mightFailSync } from "./mightFail";
 import { makeMightFail, makeMightFailSync } from "./makeMightFail";
