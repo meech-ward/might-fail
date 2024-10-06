d576bad4a60068bb50d2ce3d385a16c2ec9b3a5f
sam <sam@meech-ward.me>
2024-10-05 19:27:33 -0700

    Add an example of might and fail

---
+++ examples/mightAndFail.ts
@@ -0,0 +1,43 @@
+import { mightFail, Fail, type Either, makeMightFail } from "../src"
+
+export async function someFunction(
+  promise1: Promise<string | null> = Promise.resolve("success"),
+  promise2: Promise<number> = Promise.resolve(1)
+): Promise<Either<{ stuff: boolean }>> {
+  const [error1, result1] = await mightFail(promise1)
+  if (error1) {
+    return Fail(error1)
+  }
+  if (result1 === null) {
+    return Fail(new Error("Could not get the the thing."))
+  }
+
+  const [error2] = await mightFail(promise2)
+  if (error2) {
+    return [error2, undefined]
+  }
+
+  return [undefined, { stuff: true }]
+}
+
+// async function _someFunction(
+//   promise1: Promise<string | null> = Promise.resolve("success"),
+//   promise2: Promise<number> = Promise.resolve(1)
+// ) {
+//   const [error1, result1] = await mightFail(promise1)
+//   if (error1) {
+//     throw error1
+//   }
+//   if (result1 === null) {
+//     throw new Error("Could not get the the thing.")
+//   }
+
+//   const [error2] = await mightFail(promise2)
+//   if (error2) {
+//     throw error2
+//   }
+
+//   return { stuff: true }
+// }
+
+// export const someFunction = makeMightFail(_someFunction)
