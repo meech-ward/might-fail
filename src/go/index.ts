c06ea97ae0b6209cab78fd0a616d58c094755892
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2024-10-30 00:47:49 -0700

    chore: export NotUndefined utility type

--- src/go/index.ts
+++ src/go/index.ts
@@ -18,8 +18,9 @@
 import { type Either } from "./Either"
 import { mightFail, mightFailSync, Might, Fail } from "./mightFail"
 import { makeMightFail, makeMightFailSync } from "./makeMightFail"
+import { NotUndefined } from "../utils/utils.type"
 
-export { Either, mightFail, makeMightFail, mightFailSync, makeMightFailSync, Might, Fail }
+export { Either, mightFail, makeMightFail, mightFailSync, makeMightFailSync, Might, Fail, type NotUndefined }
 const defaultExport = {
    mightFail,
    makeMightFail,
